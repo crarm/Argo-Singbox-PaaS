1d92f64b63312c5e140bcb13ad3e0bf4ec75bb64
crarm <61692711+crarm@users.noreply.github.com>
2025-01-23 11:24:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -211,10 +211,14 @@ get_argodomain() {
 
 # 下载 Xray，并伪装 xray 执行文件
 RANDOM_NAME=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-wget -O temp.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.10.7/sing-box-1.10.7-linux-amd64.tar.gz
-tar -xzvf  temp.tar.gz
-mv sing-box-1.10.7-linux-amd64/sing-box ./${RANDOM_NAME}
-rm -f temp.tar.gz
+#wget -O temp.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.10.7/sing-box-1.10.7-linux-amd64.tar.gz
+#tar -xzvf  temp.tar.gz
+#mv sing-box-1.10.7-linux-amd64/sing-box ./${RANDOM_NAME}
+#rm -f temp.tar.gz
+wget -O temp.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip temp.zip xray geosite.dat geoip.dat
+mv xray ${RANDOM_NAME}
+rm -f temp.zip
 
 # 显示节点信息
 sleep 15
@@ -229,9 +233,6 @@ vless://${UUID}@www.digitalocean.com:443?encryption=none&security=tls&sni=${ARGO
 vmess://$(echo "{ \"v\": \"2\", \"ps\": \"Argo-Vmess\", \"add\": \"www.digitalocean.com\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO}\", \"path\": \"${WSPATH}-vmess\", \"tls\": \"tls\", \"sni\": \"${ARGO}\", \"alpn\": \"\" }" | base64 -w0)
 ----------------------------
 trojan://${UUID}@www.digitalocean.com:443?security=tls&sni=${ARGO}&type=ws&host=${ARGO}&path=%2F${WSPATH}-trojan#Argo-Trojan
-----------------------------
-ss://$(echo "chacha20-ietf-poly1305:${UUID}@www.digitalocean.com:443" | base64 -w0)@www.digitalocean.com:443#Argo-Shadowsocks
-由于该软件导出的链接不全，请自行处理如下: 传输协议: WS ， 伪装域名: ${ARGO} ，路径: /${WSPATH}-shadowsocks ， 传输层安全: tls ， sni: ${ARGO}
 *******************************************
 小火箭:
 ----------------------------
@@ -240,8 +241,6 @@ vless://${UUID}@www.digitalocean.com:443?encryption=none&security=tls&type=ws&ho
 vmess://$(echo "none:${UUID}@www.digitalocean.com:443" | base64 -w0)?remarks=Argo-Vmess&obfsParam=${ARGO}&path=/${WSPATH}-vmess&obfs=websocket&tls=1&peer=${ARGO}&alterId=0
 ----------------------------
 trojan://${UUID}@www.digitalocean.com:443?peer=${ARGO}&plugin=obfs-local;obfs=websocket;obfs-host=${ARGO};obfs-uri=/${WSPATH}-trojan#Argo-Trojan
-----------------------------
-ss://$(echo "chacha20-ietf-poly1305:${UUID}@www.digitalocean.com:443" | base64 -w0)?obfs=wss&obfsParam=${ARGO}&path=/${WSPATH}-shadowsocks#Argo-Shadowsocks
 *******************************************
 Clash:
 ----------------------------
@@ -250,8 +249,6 @@ Clash:
 - {name: Argo-Vmess, type: vmess, server: www.digitalocean.com, port: 443, uuid: ${UUID}, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: /${WSPATH}-vmess, headers: {Host: ${ARGO}}}, udp: true}
 ----------------------------
 - {name: Argo-Trojan, type: trojan, server: www.digitalocean.com, port: 443, password: ${UUID}, udp: true, tls: true, sni: ${ARGO}, skip-cert-verify: false, network: ws, ws-opts: { path: /${WSPATH}-trojan, headers: { Host: ${ARGO} } } }
-----------------------------
-- {name: Argo-Shadowsocks, type: ss, server: www.digitalocean.com, port: 443, cipher: chacha20-ietf-poly1305, password: ${UUID}, plugin: v2ray-plugin, plugin-opts: { mode: websocket, host: ${ARGO}, path: /${WSPATH}-shadowsocks, tls: true, skip-cert-verify: false, mux: false } }
 *******************************************
 EOF
 
@@ -260,6 +257,6 @@ cat list
 echo -e "\n 节点保存在文件: /app/list \n"
 echo -e "\n↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑\n"
 
-# 运行 sing-box
-#./${RANDOM_NAME} run -config config.json
+# 运行 Xray
+nohup ./${RANDOM_NAME} run -config config.json >/dev/null 2>&1 &
 
